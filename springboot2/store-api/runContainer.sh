f6d1fc1ca429c7da910f8131f90c8604ca9c20c8
joao tiago viegas <joao.viegas@maersk.com>
2019-05-22 23:42:05 +0200

    added docker image build and push to dockerhub from maven

--- springboot2/store-api/runContainer.sh
+++ springboot2/store-api/runContainer.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+NAME=store-api
+IMG=$NAME
+IMG_VERSION=0.0.1-SNAPSHOT
+CONTAINER=$NAME
+DOCKER_HUB_REPOSITORY=caquicode
+DOCKER_HUB_IMG=$DOCKER_HUB_REPOSITORY/$IMG:$IMG_VERSION
+PORT=7700
+DEBUG_PORT=7600
+
 this_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 parent_folder=$(dirname $this_folder)
 
-# include file
-. $this_folder/include.sh
 
 echo "going to run container from image $IMG"
 docker rm $CONTAINER
-docker run -d --name $CONTAINER -p $PORT:$PORT $IMG:$IMG_VERSION
+docker run -d --name $CONTAINER -p $PORT:$PORT -p $DEBUG_PORT:$DEBUG_PORT -e DEBUG_PORT=$DEBUG_PORT -e PORT=$PORT $DOCKER_HUB_IMG
 
 echo "... done."
